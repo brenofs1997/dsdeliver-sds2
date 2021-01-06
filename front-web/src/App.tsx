f5b0ccb2194cc67210a00ce5e5671f9aca51e2c8
PauloFH99 <paulo-henriquegt@hotmail.com>
2021-01-06 11:23:49 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,15 @@
+
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+
+
+function App() {
+  return (
+    <div className="">
+      <h1>Ola</h1>
+    </div>
+  );
+}
+
+export default App;

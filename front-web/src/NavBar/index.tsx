5e20555a9afade87bfab637f5acaeebd91ff60e6
PauloFH99 <brenofs1997@gmail.com>
2021-01-08 22:15:38 -0300

    Search order location

--- front-web/src/NavBar/index.tsx
+++ front-web/src/NavBar/index.tsx
@@ -2,7 +2,7 @@ import './styles.css';
 import { ReactComponent as Logo } from './logo.svg';
 import { Link } from 'react-router-dom';
 function NavBar() { 
-    return ( 
+    return (  
         <nav className="main-navbar">
             <Logo />
             <Link to="/" className="logo-text">
